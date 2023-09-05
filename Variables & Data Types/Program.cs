369fffc99831fe63b8909a825d1e395311f0b1e7
Ador-25 <abtahee.ador25@gmail.com>
2023-09-05 10:40:08 +0600

    Variables and Datatypes completed

---
+++ Variables & Data Types/Program.cs
@@ -0,0 +1,24 @@
+﻿string thisIsCamelCase;
+string firstName;
+/*Console.WriteLine(firstName); = ERROR NOT ASSIGNED */
+firstName = "Bob";
+Console.WriteLine(firstName);
+
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+
+// implicitly typed local variable
+var message = "Hello";
+Console.WriteLine(message);
+/*
+ * 
+ message = 2; ERROR Cannot implicitly convert type 'decimal' to 'string'
+ var message; ERROR Implicitly-typed variables must be initialized
+ */
