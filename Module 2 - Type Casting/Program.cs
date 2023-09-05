93234e635d38a0764f37a2b4d8e6c673b3ff8b10
Ador-25 <abtahee.ador25@gmail.com>
2023-09-05 17:55:22 +0600

    modile 2 & 3 recreated

--- Module 2 - Type Casting/Program.cs
+++ Module 2 - Type Casting/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Print(string str) => Console.WriteLine(str);
+
+//compiler does it for you
+//implicit casting
+
+int num = 2345;
+long longNum = num;
+Print(num + " " + longNum);
+
+// explicit casting
+double doubleNum = 323.32;
+int castedNum = (int)doubleNum;
+
+
+// Advance 
+string data = "124234324";
+int result = 0;
+if (int.TryParse(data, out result)) // pass by reference 
+    Print(result+"\t passed");
+else 
+    Print(result + "\t failed");
+
+
+// implicit casting 
+string firstName = "Bob";
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + " widgets.");
+Console.WriteLine(firstName + " sold " + widgetsSold +7+ " widgets.");
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+
+
+string a = "asdasfr";
+int b = 3;
+a = a + b;
+Console.WriteLine(a);
--- Module 3 - String Formatting/Program.cs
+++ Module 3 - String Formatting/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string firstName = "Bob";
+string greetings = "Hello";
+
+string message = $"{greetings} {firstName}!";
+
+Console.WriteLine(message);
+
+
+// avoid intermediate variables
+int version = 11;
+string updateText = "Update to Windows";
+Console.WriteLine($"{updateText} {version}!");
+
+
+string projectName = "First-Project";
+string output = $@"C:\Output\{projectName}\Data";
+Console.WriteLine(output);
+
+
+projectName = "ACME";
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine("View English Output:\n\t" + $@"c:\Exercise\{projectName}\data.txt");
+Console.WriteLine(russianMessage);
+
+
+Console.WriteLine(projectName+@"\");
