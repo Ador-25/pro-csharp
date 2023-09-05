7a57575721df320efd242737139d0f2b374fff03
Ador-25 <abtahee.ador25@gmail.com>
2023-09-05 11:14:32 +0600

    Type Casting done

--- Type Casting/Program.cs
+++ Type Casting/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿static void Print(string str) => Console.WriteLine(str);
+
+//compiler does it for you
+//implicit casting
+
+int num = 2345;
+long longNum = num;
+Print(num + " " + longNum);
+
+// explicit casting
+double doubleNum = 323.32;
+int castedNum = (int)doubleNum;
+
+
+// Advance 
+string data = "124234324";
+int result = 0;
+if (int.TryParse(data, out result)) // pass by reference 
+    Print(result+"\t passed");
+else 
+    Print(result + "\t failed");
