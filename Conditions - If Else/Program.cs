6f62697062287998ecce78cc9f77256465b0d237
Ador-25 <abtahee.ador25@gmail.com>
2023-09-05 15:16:13 +0600

    namespaces renamed with prefix Topic #count

---
+++ Conditions - If Else/Program.cs
@@ -0,0 +1,40 @@
+﻿static void GetRule(int days,out int discount)
+{
+
+    if(days == 0)
+    { 
+        discount = 0;
+        Console.WriteLine("Your subscription has expired.");
+    }
+    else if(days == 1)
+    {
+        discount = 20;
+        Console.WriteLine($"Your subscription expires within a day!\nRenew now and save {discount}%!");
+    }
+    else if(days >1 && days<=5) 
+    {
+        discount = 10;
+        Console.WriteLine($"Your subscription expires in {days} days.\nRenew now and save {discount}%!");
+    }
+    else if (days > 5 && days <= 10)
+    {
+        discount = 0;
+        Console.WriteLine("Your subscription will expire soon. Renew now!");
+    }
+    else
+    {
+        discount = 0;
+    }
+
+}
+
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+
+Console.WriteLine($"Exp: {daysUntilExpiration}");
+GetRule(daysUntilExpiration, out discountPercentage);
+Console.WriteLine(discountPercentage);
---
+++ Method Overloading/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+
+class Printer
+{
+    public void Print(int value)
+    {
+        Console.WriteLine(value);
+    }
+
+    public void Print(double value)
+    {
+        Console.WriteLine(value);
+    }
+
+    public void Print(string value)
+    {
+        Console.WriteLine(value);
+    }
+
+    public void Print(string format, params object[] args)
+    {
+        Console.WriteLine(format, args);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Printer printer = new Printer();
+
+        int intValue = 42;
+        double doubleValue = 3.14;
+        string stringValue = "Hello, World!";
+
+        // Calling the overloaded Print methods
+        printer.Print(intValue);
+        printer.Print(doubleValue);
+        printer.Print(stringValue);
+        printer.Print((doubleValue+intValue));
+
+        // Using the format and params version of Print
+        printer.Print("Formatted: {0} - {1}", doubleValue, stringValue);
+    }
+}
