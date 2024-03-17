e5cdb66d1c97f0bd1a8f6c9e8d327703540089e2
Ador-25 <abtahee.ador25@gmail.com>
2024-03-18 03:47:08 +0600

    initial commit - Introduction Project

---
+++ Introduction/Program.cs
@@ -0,0 +1,12 @@
+﻿// Setting up console UI
+Console.Title = "First App";
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.BackgroundColor = ConsoleColor.Black;
+
+Console.WriteLine("**********************************");
+Console.WriteLine("First App!:D");
+Console.WriteLine("**********************************");
+Console.BackgroundColor = ConsoleColor.Black;
+
+Console.ReadLine();
+
